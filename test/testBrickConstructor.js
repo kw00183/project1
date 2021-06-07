5fc76b94754f93e1b0b1f1c97b114d095794677a
kw00183 <kw00183@cs6261-vm02.uwg.westga.edu>
2021-06-06 21:50:36 -0400

    remove extra var assert in testBrickConstructor.js file

--- test/testBrickConstructor.js
+++ test/testBrickConstructor.js
@@ -1,1 +1,13 @@
+var assert = require('assert');
+var lego = require('../legos');
+const Brick = lego.Brick;
+
+describe('Brick', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([2, "blue"].indexOf(4), -1);
+    });
+  });
+});
+
 
