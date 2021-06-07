44fab0348c4436afc696e2ddcf822139b8f4fb09
kw00183 <kw00183@cs6261-vm03.uwg.westga.edu>
2021-06-07 13:16:49 -0400

    add in LegoPile constructor to legos.js file as well as insert function accepts a Brick and stores the brick into the color's BST

--- legos.js
+++ legos.js
@@ -1,6 +1,17 @@
 module.exports.Brick = Brick;
+module.exports.LegoPile = LegoPile;
 
 function Brick(size, color) {
   this.size = size;
   this.color = color;
+}
+
+function LegoPile() {
+  this.dictionary = new Array();
+  this.insert = insert;
+}
+
+function insert(Brick) {
+  var bst = dictionary[Brick.color];
+  bst.insert(Brick);
 }
